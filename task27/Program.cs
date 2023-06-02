341536f61aaaabfa0e454de9832bff50c5e42048
KirichenkoAlexandr <magnetta1990@gmail.com>
2023-06-02 12:38:22 +0300

    added task27

---
+++ task27/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (x > 0)
+{
+   int num  = x % 10;
+   x= x/10;
+   sum = sum+num;
+
+}
+Console.WriteLine($"сумма всех цифр в числе = {sum}");
