be823be8cecedc3f375cab1dc28b2471c01e8d8d
KirichenkoAlexandr <magnetta1990@gmail.com>
2023-06-12 13:41:43 +0300

    added task

---
+++ task28/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача28.
+// Напиши программу которая принимант на вход число N 
+// и выдает произведение чисел от 1 доN.
+
+int Factorial(int num)
+{
+    int result = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            result *= i;
+        }
+    }
+    return result;
+}
+
+int Start(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = Start("Введите целое положительное число");
+if (number <= 0)
+{
+    Console.WriteLine("Вы ввели некорректное число");
+    return;
+}
+Console.WriteLine(Factorial(number));
---
+++ task30/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] array = new int[8];
+
+int[] result = FillArray(array);
+
+PrintArray(result);
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write("]");
+    Console.WriteLine("");
+}
+
+int[] FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = CreateRandom();
+    }
+    return array;
+}
+
+int CreateRandom()
+{
+    return new Random().Next(2);
+}
+
+
+
+// void FillArray(int[] array)
+//             {
+//                 var rnd = new Random();
+//                 for (int i = 0; i < array.Length; i++)
+//                 {
+//                     array[i] = rnd.Next(2);
+//                 }
+//             }
+
+
+//             void PrintArray(int[] array)
+//             {
+//                 for (int i = 0; i < array.Length; i++)
+//                 {
+//                     Console.Write(array[i] + " ");
+//                 }
+//             }
+
+//             int[] array1 = new int[8];
+//             FillArray(array1);
+//             PrintArray(array1);
